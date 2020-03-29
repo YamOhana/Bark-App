da562fa7b04e616d02cd973d7d748823b6719183
YamOhana <yamoh1993@gmail.com>
2020-03-29 22:42:53 +0300

    componenets

---
+++ src/components/Chatroom.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+class ChatRoom extends Component{
+
+
+render(){
+
+ 
+
+    
+    return(
+
+
+dfdf
+    )
+
+}
+
+}
+
+
+export default ChatRoom
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,75 @@
-import React, {component, Component, Profiler} from 'react'
+import React, { component, Component, Profiler } from 'react'
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+import AddDog from './AddDog';
+import Mainstore from '../Stores/MainStore'
+import { Layout, Divider, Avatar, Icon, Button } from 'antd';
 
 
 
-class Profile extends Component{
+const { Header, Footer, Sider, Content } = Layout;
 
 
+@inject("MainStore")
+
+class Profile extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            dogList: true,
+            dogs: []
+
+        }
+    }
+
+    onToggle = () => {
+        let opposite = !this.state.dogList
+        this.setState({
+            dogList: opposite
+        })
+    }
+
+    onChange = (e) => {
+        console.log(`checked = ${e.target.checked}`);
+    }
+
+
+    componentDidMount = async () =>{
+        const currUserId = this.props.ownerStore.currUser.id
+        await this.props.Mainstore.getOwnerDogs(currUserId)
+    }
+
+
+
+    render(){
+
+        const state = this.state
+
+            return(
+                <div className="ProfileComponent">
+                <Layout id="profileLayout" style={{ height: "100vh" }}>
+                    <Header id="header" >
+                    <div id="profileImgDiv">
+                    </div>
+                    </Header>
+
+                    <Content className="profileContent">
+
+                        <Route exact path="/owner/dog${ownerId}"> </Route>
+                        
+                        <Route path="/owner/dog{ownerId}" exact render={({match}) => <ProfileList match = {match} />} />
+                        <Route path="/owner/dog{ownerId}" exact render={({match}) => <AddDog match = {match}/>} />
+
+                    </Content>
+                </Layout>
+
+
+            </div>
+
+
+            )
+
+    }
 
 
 }
