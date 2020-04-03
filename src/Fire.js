803cf644df432247f34733b5f3b231f0db2273c9
YamOhana <yamoh1993@gmail.com>
2020-04-03 20:25:41 +0300

    chat

--- src/Fire.js
+++ src/Fire.js
@@ -8,11 +8,18 @@ const firebaseConfig = {
     storageBucket: "bark-hackathon.appspot.com",
     messagingSenderId: "895115782826",
     appId: "1:895115782826:web:adb0624854db0cd762fed3",
-    measurementId: "G-WD5K5VC19E"
+    measurementId: "G-WD5K5VC19E", 
+    messagingSenderId: '895115782826'
 };
 
 // Initialize Firebase
 const fire = firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
+
+//for chat
+firebase.firestore().settings({
+    timestampsInSnapshots: true
+})
+
 
 export default fire
